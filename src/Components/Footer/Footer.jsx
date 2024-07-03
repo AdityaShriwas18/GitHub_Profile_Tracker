3aba0f0c3b22b72262c92a540dfb3f767c8f7ea6
AdityaShriwas18 <147526826+AdityaShriwas18@users.noreply.github.com>
2024-07-03 07:49:48 -0700

    GitHub Profile Finder

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,10 +7,10 @@ function Footer() {
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                    <div className='flex gap-2'>
+                        <img src="GPF.png" className="h-10 rounded-3xl" alt="GitHub Profile Finder" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GitHub Profile Finder</span>
-                    </a>
+                    </div>
                 </div>
                 <div className='flex flex-wrap'>
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 GitHub_Profile_Finder™.</span>
--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -14,8 +14,8 @@ function Navbar() {
 
     return (
         <>
-            <nav className='sticky top-0 w-full p-4 bg-white/20 backdrop-blur-md shadow-md flex justify-between items-center z-1100'>
-                <h1 className='text-xl'><img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />GPF</h1>
+            <nav className='sticky top-0 w-full p-2 bg-white/20 backdrop-blur-md shadow-md flex justify-between items-center z-1100'>
+                <h1 className='text-xl'><img src="GPF.png" className="h-12 rounded-3xl" alt="Flowbite Logo" /></h1>
             </nav>
 
             <div onClick={toggleOpen} className={`hamburger ${isOpen ? 'open' : ''}`} id="hamburger">
@@ -24,10 +24,10 @@ function Navbar() {
                 <div className="bar3"></div>
             </div>
 
-            <div className={`navbar fixed top-0 right-0 w-[250px] h-[100%] bg-[rgba(190,175,175,0.387)] backdrop-blur-sm transform transition-transform duration-300 ease-in-out z-[1000] pt-[60px] ${isOpen ? 'translate-x-0' : 'translate-x-full'}`} id="navbar">
+            <div className={`navbar fixed top-0 right-0 w-[250px] h-[100%] bg-[rgba(190,175,175,0.387)] backdrop-blur-sm transform transition-transform duration-300 ease-in-out z-[1000] pt-[100px] ${isOpen ? 'translate-x-0' : 'translate-x-full'}`} id="navbar">
                 <NavLink to='/' className={({ isActive }) => `${isActive ? "bg-[#1D1D2F]" : "text-white"} block px-5 py-2.5 text-[20px] no-underline transition-colors duration-300 hover:bg-[#918e8e]`} onClick={closeNavbar}>Home</NavLink>
                 <NavLink to='/about' className={({ isActive }) => `${isActive ? "bg-[#1D1D2F]" : "text-white"} block px-5 py-2.5 text-[20px] no-underline transition-colors duration-300 hover:bg-[#918e8e]`} onClick={closeNavbar}>About</NavLink>
-                <a href='https://github.com/AdityaShriwas18' className='block px-5 py-2.5 text-[20px] text-white no-underline transition-colors duration-300 hover:bg-[#918e8e]' onClick={closeNavbar}>GitHub</a>
+                <a href='https://github.com/AdityaShriwas18/GitHub_Profile_Finder' className='block px-5 py-2.5 text-[20px] text-white no-underline transition-colors duration-300 hover:bg-[#918e8e]' onClick={closeNavbar}>GitHub</a>
             </div>
         </>
     );
