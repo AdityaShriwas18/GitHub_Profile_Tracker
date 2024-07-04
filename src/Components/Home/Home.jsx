346ca4768b1e6b66ea4ea687420b8d01d226c9d8
AdityaShriwas18 <147526826+AdityaShriwas18@users.noreply.github.com>
2024-07-04 03:43:14 -0700

    added blog link

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -21,6 +21,7 @@ function Home() {
     let [public_repos, setPublic_repos] = useState(0)
     let [error, setError] = useState(null);
     let [loading, setLoading] = useState(false);
+    const [blog, setBlog] = useState('Blog')
 
     let API = "https://api.github.com/users/"
 
@@ -56,6 +57,7 @@ function Home() {
                 setEmail(data.email || 'Not available')
                 setType(data.type || 'Not available')
                 setError(null);
+                setBlog(data.blog)
             } catch (error) {
                 console.error(error.message);
                 setError('Inavlid Username or User Not exist!');
@@ -130,6 +132,15 @@ function Home() {
                                     </svg>
                                     <p class="text-gray-100">{location}</p>
                                 </div>
+                                
+                                <div class="flex items-center space-x-1">
+                                <svg aria-hidden="true" fill='white' height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-link">
+                                    <path d="m7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z"></path>
+                                </svg>
+                                    <a href={blog} class="text-gray-100">{blog}</a>
+                                </div>
+
+                                
 
                                 <div v-if="infos.twitter_username" class="flex items-center space-x-1">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 273.5 222.3" width="16" height="16">
