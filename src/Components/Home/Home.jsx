197e4912efc5ed9520d4511d7eb4835c5efa85f7
Aditya Shriwas <161008667+AdityaShriwas18@users.noreply.github.com>
2024-08-31 06:43:02 -0700

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -79,7 +79,7 @@ function Home() {
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
-                        <input type="search" value={inputValue} onKeyUp={(e) => { if (e.key === 'Enter') getuserdata() }} onChange={(e) => setInputValue(e.target.value)} id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search User Profile..." required />
+                        <input type="search" value={inputValue} onKeyUp={(e) => { if (e.key === 'Enter') getuserdata() }} onChange={(e) => setInputValue(e.target.value)} id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter UserID..." required />
                         <button
                             onClick={getuserdata}
 
